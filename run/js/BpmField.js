f12d0a6aaceb98bf27c93e1cf5c12d89bea57cc9
Archil Imnadze <archil@imnadze.ge>
2014-08-18 10:31:48 +0000

    css relayout

--- run/js/BpmField.js
+++ run/js/BpmField.js
@@ -5,7 +5,10 @@ function BpmField () {
     var labelElement = Div(classPrefix + '-label')
     labelElement.appendChild(TextNode('BPM:'))
 
+    var valueNode = TextNode('0')
+
     var valueElement = Div(classPrefix + '-value')
+    valueElement.appendChild(valueNode)
 
     var element = Div(classPrefix)
     element.appendChild(labelElement)
--- run/js/IntervalField.js
+++ run/js/IntervalField.js
@@ -5,7 +5,10 @@ function IntervalField () {
     var labelElement = Div(classPrefix + '-label')
     labelElement.appendChild(TextNode('INTERVAL:'))
 
+    var valueNode = TextNode('0ms')
+
     var valueElement = Div(classPrefix + '-value')
+    valueElement.appendChild(valueNode)
 
     var element = Div(classPrefix)
     element.appendChild(labelElement)
