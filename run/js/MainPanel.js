6745e0555bc8f9368078546d69d0fe5048216e68
Archil Imnadze <archil@imnadze.ge>
2014-08-18 11:31:53 +0000

    initial blink

--- run/js/MainPanel.js
+++ run/js/MainPanel.js
@@ -1,24 +1,38 @@
 function MainPanel () {
 
+    function scheduleTick () {
+        setTimeout(function () {
+            blink.tick()
+            scheduleTick()
+        }, averageInterval)
+    }
+
     var classPrefix = 'MainPanel'
 
+    var averageInterval = 500
+
     var bpmField = BpmField()
+    bpmField.setValue(Math.round(60000 / averageInterval))
 
     var intervalField = IntervalField()
+    intervalField.setValue(averageInterval)
 
     var lastTime = null
     var intervals = []
 
+    var blink = Blink()
+
     var tapButton = TapButton(function () {
         var now = Date.now()
         if (lastTime !== null) {
-            intervals.push(now - lastTime)
+            var interval = now - lastTime
+            intervals.push(interval)
             if (intervals.length > 4) intervals.shift()
             var sum = intervals.reduce(function (a, b) {
                 return a + b
             })
-            var averageInterval = sum / intervals.length
-            intervalField.setValue(Math.round(averageInterval))
+            averageInterval = Math.round(sum / intervals.length)
+            intervalField.setValue(averageInterval)
             bpmField.setValue(Math.round(60000 / averageInterval))
         }
         lastTime = now
@@ -27,7 +41,10 @@ function MainPanel () {
     var element = Div(classPrefix)
     element.appendChild(bpmField.element)
     element.appendChild(intervalField.element)
+    element.appendChild(blink.element)
     element.appendChild(tapButton.element)
+
+    scheduleTick()
 
     return { element: element }
 
