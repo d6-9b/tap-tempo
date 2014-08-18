46499dd85904095f91cb4836411f748c7a463d4b
Archil Imnadze <archil@imnadze.ge>
2014-08-18 23:25:21 +0400

    fixed audio overlapping issue

--- run/js/Main.js
+++ run/js/Main.js
@@ -4,15 +4,20 @@
     request.responseType = 'arraybuffer'
     request.send()
     request.onload = function () {
+
+        var audioBufferSource
+
         var audioContext = new AudioContext
         audioContext.decodeAudioData(request.response, function (audioBuffer) {
             var mainPanel = MainPanel(function () {
-                var audioBufferSource = audioContext.createBufferSource()
+                if (audioBufferSource) audioBufferSource.stop()
+                audioBufferSource = audioContext.createBufferSource()
                 audioBufferSource.buffer = audioBuffer
                 audioBufferSource.connect(audioContext.destination)
                 audioBufferSource.start()
             })
             document.body.appendChild(mainPanel.element)
         })
+
     }
 })()
