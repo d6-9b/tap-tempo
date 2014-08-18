dbcf5dccc8c778d5b7bfaa5de101d19a2e64b57a
Archil Imnadze <archil@imnadze.ge>
2014-08-18 22:47:03 +0400

    mute button mouse compatibility

--- run/js/MuteButton.js
+++ run/js/MuteButton.js
@@ -1,7 +1,55 @@
 function MuteButton () {
 
+    function press () {
+        classList.add('active')
+    }
+
+    function release () {
+        classList.remove('active')
+    }
+
     var element = Div('MuteButton')
     element.appendChild(TextNode('MUTE'))
+    element.addEventListener('mousedown', function (e) {
+
+        function mouseUp (e) {
+            e.preventDefault()
+            release()
+            removeEventListener('mouseup', mouseUp)
+        }
+
+        e.preventDefault()
+        if (touched) touched = false
+        else {
+            press()
+            addEventListener('mouseup', mouseUp)
+        }
+
+    })
+    element.addEventListener('touchstart', function (e) {
+        e.preventDefault()
+        touched = true
+        if (identifier !== null) return
+        var touch = e.changedTouches[0]
+        identifier = touch.identifier
+        press()
+    })
+    element.addEventListener('touchend', function (e) {
+        e.preventDefault()
+        touched = true
+        var touches = e.changedTouches
+        for (var i = 0; i < touches.length; i++) {
+            if (touches[i].identifier === identifier) {
+                identifier = null
+                release()
+            }
+        }
+    })
+
+    var classList = element.classList
+
+    var touched = false,
+        identifier = null
 
     return {
         element: element,
