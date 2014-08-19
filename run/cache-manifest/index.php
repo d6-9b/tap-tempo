e8ecb59dd4f68334c56150b5406ba18cdd9d2092
Archil Imnadze <archil@imnadze.ge>
2014-08-19 07:12:08 +0000

    added cache manifest

---
+++ run/cache-manifest/index.php
@@ -0,0 +1,9 @@
+<?php
+
+header('Content-Type: text/cache-manifest');
+
+echo
+    "CACHE MANIFEST\n"
+    ."../compressed.css\n"
+    ."../compressed.js\n"
+    ."../pulse.ogg\n";
--- run/index.php
+++ run/index.php
@@ -5,4 +5,4 @@ $head = '<link rel="stylesheet" type="text/css" href="compressed.css" />';
 $body = '<script type="text/javascript" src="compressed.js"></script>';
 
 include_once 'fns/echo_html.php';
-echo_html('<html>', $head, $body);
+echo_html('<html manifest="cache-manifest/">', $head, $body);
