4c4a43b1e4bb99db618075b8089c46c374743113
Archil Imnadze <archil@imnadze.ge>
2015-04-08 11:44:04 +0000

    embed all-size icons in html pages

--- fns/get_revisions.php
+++ fns/get_revisions.php
@@ -5,6 +5,10 @@ function get_revisions () {
         'images/icons/16.png' => 1,
         'images/icons/32.png' => 1,
         'images/icons/64.png' => 1,
+        'images/icons/90.png' => 1,
+        'images/icons/120.png' => 1,
         'images/icons/128.png' => 1,
+        'images/icons/256.png' => 1,
+        'images/icons/512.png' => 1,
     ];
 }
